d30e4de51f02beb690bf23408983c576d0749656
CRD1998 <60524724+CRD1998@users.noreply.github.com>
2020-02-25 22:51:06 +0000

    Time-varying magnetic field

--- Project/EMField.py
+++ Project/EMField.py
@@ -5,11 +5,20 @@ import math
 from System import system
 
 class EMField:
+    """
+    Class to represent an  electromagnetic field.
+
+    Parse in the electric field at its maximum and it will be converted to a time-varying
+    electric field of the form Acos(wt+phi) 
+
+    Electric field in N/C or V/m
+    Magnetic field in T
+    """
 
     def __init__(self, ElectricField = np.array([0,0,0], dtype=float), 
                 MagneticField = np.array([0,0,0], dtype=float)):
-        self.electric = np.array(ElectricField,dtype=float)
         self.magnetic = np.array(MagneticField,dtype=float)
+        self.electric = np.array(ElectricField,dtype=float)
         log.logger.info('electromagentic field generated')
 
     def __repr__(self):
@@ -19,14 +28,18 @@ class EMField:
         return np.linalg.norm(self.electric)
     
     def magneticMag(self):
-        return np.linalg.norm(self.magneticMag)
+        return np.linalg.norm(self.magnetic)
     
-    def getAcceleration(self,particle):
-        lorentz = np.array([0,0,0],dtype=float)
-        if particle.position[0] > -0.98:
-            lorentz += -1*self.electric
-        if particle.position[0] < 0.098:
-            lorentz += self.electric
+    def frequency(self,particle):
+        return abs(particle.charge)*self.magneticMag()/particle.mass
+
+    def ImplementElectricField(self,particle,time):
+        if particle.position[0] > -0.98 and particle.position[0] < 0.98:
+            return np.array([i*math.cos(self.frequency(particle)*time) for i in self.electric],dtype=float)
+        return [0,0,0]
+
+    def getAcceleration(self,particle,time):
+        lorentz = self.ImplementElectricField(particle,time)
         lorentz += np.cross(particle.velocity, self.magnetic)
         lorentz *= (particle.charge / particle.mass)
         particle.acceleration = lorentz
--- Project/main.py
+++ Project/main.py
@@ -7,31 +7,48 @@ import scipy.constants as const
 import matplotlib.pyplot as plt
 import copy
 
-proton = ChargedParticle('proton-1', const.m_p, const.e, [0,0,0], [0,3000,0])
-field = EMField([0,0.1,0], [0,0,1.6*10**(-5)])
+proton = ChargedParticle('proton-1', const.m_p, const.e, [0,0,0], [3000,0,0])
+field = EMField([0.1,0,0], [0,0,1.6*10**(-5)])
 
 time, deltaT, duration = 0, 10**(-5), 0.03
 
 Data = []
+electricField = []
+timeSeries = []
 
 log.logger.info('starting simulation')
 while time <= duration:
     time += deltaT
-    field.getAcceleration(proton)
+    timeSeries.append(time)
+    field.getAcceleration(proton,time)
     proton.eulerCromer(deltaT)
-    temp_particle= copy.deepcopy(proton)
+    temp_particle, temp_field= copy.deepcopy(proton), copy.deepcopy(field.ImplementElectricField(proton,time))
     Data.append(temp_particle.position)
+    electricField.append(temp_field)
 log.logger.info('simulation finished')
 
 log.logger.info('creating plots')
 x, y = [i[0] for i in Data], [i[1] for i in Data]
-magneticX, magneticY = np.meshgrid(list(range(-1,6)), list(range(-3,4)))
+final = [x[-1], y[-1]]
+magneticX, magneticY = np.meshgrid(list(range(-15,21,5)), list(range(-20,16,5)))
+
+#caption = r' For this cyclotron: $\vec{B}=(1.6\times 10^{-5})\mathbf{\hat{e}_z}$ $T$ , $\vec{E}=0.1\mathbf{\hat{e}_y}$ $NC^{-1}$'
 
 plt.figure('Proton in EM Field')
+#plt.figtext(0.5, 0.01, caption, wrap=True, horizontalalignment='center', fontsize=10)
 plt.plot(x,y,color='blue',label="Euler Cromer Path")
-
+plt.scatter(final[0],final[1],color='blue',marker='o')
 plt.xlabel(r'x  position  [$m$]')
 plt.ylabel(r'y  position [$m$]')
 plt.title('Plot to show the path of a proton in an electromagnetic field')
-plt.legend()
+plt.axvspan(-0.98,0.98,alpha=0.5, color='grey',label='Electric Field')
+plt.scatter(magneticX,magneticY,marker=r'$\bigotimes$',s=95,color='black',label='Magnetic Field')
+plt.legend(loc='upper right',framealpha=1)
+
+plt.figure('Electric Field')
+plt.plot(timeSeries,[i[0] for i in electricField],label='x component of electric field')
+plt.xlabel('time [$s$]')
+plt.ylabel('Electric Field [$NC^{-1}$]')
+plt.title(r'Plot to show $E_{x}$ of $\vec{E}$')
+
 plt.show()
