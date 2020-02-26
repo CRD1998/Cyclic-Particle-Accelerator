8a84c4567637ac70657f5527d0aaf23304f914de
CRD1998 <60524724+CRD1998@users.noreply.github.com>
2020-02-26 13:51:14 +0000

    Updated plots
    Inital proton now stars at rest in the centre of the magnetic field

--- Project/main.py
+++ Project/main.py
@@ -7,7 +7,7 @@ import scipy.constants as const
 import matplotlib.pyplot as plt
 import copy
 
-proton = ChargedParticle('proton-1', const.m_p, const.e, [0,0,0], [3000,0,0])
+proton = ChargedParticle('proton-1', const.m_p, const.e, [0,0,0], [0,0,0])
 field = EMField([0.1,0,0], [0,0,1.6*10**(-5)])
 
 time, deltaT, duration = 0, 10**(-5), 0.03
@@ -27,16 +27,18 @@ while time <= duration:
     electricField.append(temp_field)
 log.logger.info('simulation finished')
 
+log.logger.info("{0}'s final linear speed: {1} m/s".format(proton.name,int(proton.magnitude(proton.velocity))))
+
 log.logger.info('creating plots')
 x, y = [i[0] for i in Data], [i[1] for i in Data]
 final = [x[-1], y[-1]]
-magneticX, magneticY = np.meshgrid(list(range(-15,21,5)), list(range(-20,16,5)))
+magneticX, magneticY = np.meshgrid(list(range(-20,21,5)), list(range(-20,16,5)))
 
 plt.figure('Cyclotron')
 plt.plot(x,y,color='blue',label="Euler Cromer Path")
 plt.scatter(final[0],final[1],color='blue',marker='o')
-plt.xlabel(r'x  position  [$m$]')
-plt.ylabel(r'y  position [$m$]')
+plt.xlabel(r'x  position  [m]')
+plt.ylabel(r'y  position [m]')
 plt.title('Plot to show the path of a proton in a cyclotron')
 plt.axvspan(-0.98,0.98,alpha=0.5, color='grey',label='Electric Field')
 plt.scatter(magneticX,magneticY,marker=r'$\bigotimes$',s=95,color='black',label='Magnetic Field')
