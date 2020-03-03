52ad9eeac36a82149de010fe0492ebfbafb27e74
CRD1998 <60524724+CRD1998@users.noreply.github.com>
2020-03-03 23:09:14 +0000

    Better plots

--- Project/main.py
+++ Project/main.py
@@ -7,13 +7,13 @@ from EMField import EMField
 from ProtonBunch import ProtonBunch
 
 field = EMField([.1,0,0], [0,0,1.6*10**(-5)]) 
-protons = ProtonBunch(0.047,5)
+protons = ProtonBunch(0.047)
 
 log.logger.info('Initial average kinetic energy: %s eV' % protons.KineticEnergy())
 log.logger.info('Initial average momentum: %s kg m/s' % protons.momentum())
 log.logger.info('Initial bunch spread: %s m' % protons.spread())
 
-time, deltaT, duration = 0, 10**(-6), 0.0041
+time, deltaT, duration = 0, 10**(-6), 0.015
 
 timeSeries = []
 Data = []
@@ -25,31 +25,39 @@ while time <= duration:
     field.getAcceleration(protons.bunch, time, deltaT)
     temp_bunch = copy.deepcopy(protons)
     Data.append(temp_bunch)
+
 log.logger.info('simulation finished')
+log.logger.info('building lists')
 
-#magneticX, magneticY = np.meshgrid(list(range(-20,21,5)), list(range(-20,16,5)))
-
-log.logger.info('creating plots')
-plt.figure('Cyclotron with Bunch')
-
-plt.axvspan(field.electricLowerBound,field.electricUpperBound,alpha=0.5, color='grey',label='Electric Field')
-#plt.scatter(magneticX,magneticY,marker=r'$\bigotimes$',s=95,color='black',label='Magnetic Field')
-
-x,y,y_downSpread,y_upSpread, x_spread, y_spread = [],[],[],[],[],[]
+x,y,y_downSpread,x_upSpread,x_downSpread,y_upSpread,x_spread,y_spread = [],[],[],[],[],[],[],[]
 for bunch in Data:
     x.append(bunch.averagePosition()[0])
     y.append(bunch.averagePosition()[1])
+
+    x_downSpread.append(bunch.averagePosition()[0] - bunch.spread()[0])
+    x_upSpread.append(bunch.averagePosition()[0] + bunch.spread()[0])
     y_downSpread.append(bunch.averagePosition()[1] - bunch.spread()[1])
     y_upSpread.append(bunch.averagePosition()[1] + bunch.spread()[1])
+
     x_spread.append(bunch.spread()[0])
     y_spread.append(bunch.spread()[1])
 final = [x[-1], y[-1]]
-plt.plot(x,y,label=protons.bunchName)
-plt.scatter(final[0], final[1], marker='.')
-plt.fill_between(x,y_downSpread,y_upSpread)
+magneticX, magneticY = np.meshgrid(list(range(-10,11,5)), list(range(-15,11,5)))
+
+
+log.logger.info('creating plots')
+plt.figure('Cyclotron Bunch with Spread')
+
+plt.axvspan(field.electricLowerBound,field.electricUpperBound,alpha=0.5, color='grey',label='Electric Field')
+plt.scatter(magneticX,magneticY,marker=r'$\bigotimes$',s=95,color='black',label='Magnetic Field')
+plt.plot(x,y,label=protons.bunchName,color='blue')
+plt.scatter(final[0], final[1],color='blue')
+plt.fill_between(x,y_downSpread,y_upSpread, color='cornflowerblue',label="spread")
+plt.fill_betweenx(y, x_downSpread, x_upSpread, color='cornflowerblue')
 plt.xlabel(r'x  position  [m]')
 plt.ylabel(r'y  position [m]')
 plt.legend(loc='best',framealpha=1)
+
 
 plt.figure('Spread')
 plt.plot(timeSeries,x_spread,label=r'$\sigma_{x}$')
